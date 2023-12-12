94a6afa76e0385e41e9c037662b485c878c09d4e
Nathan Riley <nve2@protonmail.com>
2023-12-11 23:26:01 -0500

    Remove duplicates

--- 11-13-2023.sql
+++
@@ -1,8 +0,0 @@
--- find out how much money it will cost to give salesmen a salary increase of 2%, clerks 4%, and everyone else of 6%
-select sum(
-  salary * (case when job = 'SALESMAN' then 1.02
-      else (case when job = 'CLERK' then 1.04 
-      else 1.06 end
-    )
-  ) as total_salary_increase
-  from emp;
